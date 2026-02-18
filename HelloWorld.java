40cf25acbdf37b1ca60ecf79294ae76ac5f4022d
Jobin Sunny <66160325+jobinsunny@users.noreply.github.com>
2026-02-18 06:52:29 +0530

    Create HelloWorld.java file with initial implementation

---
+++ HelloWorld.java
@@ -0,0 +1,1 @@
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, World!"); System.out.println("CI/CD Pipeline Test"); } public static String greet(String name) { return "Hello, " + name + "!"; } }
